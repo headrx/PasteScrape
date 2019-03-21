7827c3820fde92ba8f94416e10d7581631934555
headrx <joshbelmar@gmail.com>
2019-03-21 13:15:20 -0500

    Re-Organized into functions

--- pastebin.py
+++ pastebin.py
@@ -4,31 +4,40 @@ from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import webbrowser
 
-#WIll store all pastes from the /archive(recent)
-all_pastes = {}
 
-#Selenium driver setup
-chrome_options = Options()
-#chrome_options.add_argument('--headless')
-driver = webdriver.Chrome(options=chrome_options)
+def retrieve_pastes():
+        #WIll store all pastes from the /archive(recent)
+        all_pastes = {}
 
-#Request the page
-driver.get('https://pastebin.com/archive')
-#Get the source and open in soup
-page_source = driver.page_source
-soup = BeautifulSoup(page_source, 'lxml')
-driver.quit()
-#Find our table and grab all links
-latest_pastes = soup.find('table', class_="maintable")
-pastes = latest_pastes.findAll('a')
-#add each link to all_pastes as post_title:url
-for paste in pastes:
-    all_pastes[paste.text] = "https://pastebin.com/raw"+paste['href']
-print('[+] Complete')
-print('[+] Opening...')
+        #Selenium driver setup
+        chrome_options = Options()
+        #chrome_options.add_argument('--headless')
+        driver = webdriver.Chrome(options=chrome_options)
 
-#Open all links in browser
-for paste in all_pastes:
-    if not all_pastes[paste].startswith('https://pastebin.com/raw/archive'):
-        print("[+] URL Opened >> ", all_pastes[paste])
-        webbrowser.open(all_pastes[paste])
+        #Request the page
+        driver.get('https://pastebin.com/archive')
+        #Get the source and open in soup
+        page_source = driver.page_source
+        soup = BeautifulSoup(page_source, 'lxml')
+        driver.quit()
+        #Find our table and grab all links
+        latest_pastes = soup.find('table', class_="maintable")
+        pastes = latest_pastes.findAll('a')
+        #add each link to all_pastes as post_title:url
+        for paste in pastes:
+                all_pastes[paste.text] = "https://pastebin.com/raw"+paste['href']
+
+        print('[+] Complete')
+        print('[+] Opening...')
+        return all_pastes
+
+
+def open_pastes(all_pastes):
+        #Open all links in browser
+        for paste in all_pastes:
+                if not all_pastes[paste].startswith('https://pastebin.com/raw/archive'):
+                        print("[+] URL Opened >> ", all_pastes[paste])
+                        webbrowser.open(all_pastes[paste])
+
+pastes = retrieve_pastes()
+open_pastes(pastes)
