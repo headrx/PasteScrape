7f531ce65db02c18c812bf34a3693da4f8a661c5
headrx <joshbelmar@gmail.com>
2019-04-04 13:16:21 -0500

    Added the server upload function, changed view of paste to default view (non-raw), and commented code

--- pastebin.py
+++ pastebin.py
@@ -3,6 +3,7 @@ import webbrowser, requests
 
 
 def retrieve_pastes():
+        """Scrape the newest public pastes """
         #WIll store all pastes from the /archive(recent)
         all_pastes = {}
         #Request the page
@@ -17,17 +18,19 @@ def retrieve_pastes():
         pastes = latest_pastes.findAll('a')
         #add each link to all_pastes as post_title:url
         for paste in pastes:
-                all_pastes[paste.text] = "https://pastebin.com/raw"+paste['href']
+                all_pastes[paste.text] = "https://pastebin.com"+paste['href']
 
         print('[+] Complete')
         print('[+] Opening...')
         return all_pastes
 
 def write_paste_log(url):
+        """ Write urls to txt file to keep from viewing duplicates"""
         with open('checked_pastes.txt', 'a') as paste_file:
                 paste_file.writelines(url+"\n")
 
 def check_log(paste_url):
+        """Check the log of viewed urls"""
         with open('checked_pastes.txt', 'r') as paste_file:
                 paste_file = paste_file.readlines()
         if paste_url not in paste_file:
@@ -36,12 +39,35 @@ def check_log(paste_url):
                 return False
 
 def open_pastes(all_pastes):
+        """Open all the pastes in browser tabs """
         #Open all links in browser
         for paste in all_pastes:
-                if not all_pastes[paste].startswith('https://pastebin.com/raw/archive') and check_log(all_pastes[paste]):
-                        print("[+] URL Opened >> ", all_pastes[paste])
-                        write_paste_log(all_pastes[paste])
-                        webbrowser.open(all_pastes[paste])
+                if 'archive' not in all_pastes[paste]:
+                        if check_log(all_pastes[paste]) == True:
+                                #print("[+] URL Opened >> ", all_pastes[paste])
+                                write_paste_log(all_pastes[paste])
+                                webbrowser.open(all_pastes[paste])
+
+def submit_pastes(pastes):
+        """This will ask user to submit specific pastes they find worth submission"""
+        x = 0
+        paste_menu_selections = {}
+        for paste in pastes:
+                if 'archive' not in pastes[paste]:
+                        print("[{}] {} : {}".format(x,paste,pastes[paste]))
+                        paste_menu_selections[str(x)] = pastes[paste]
+                        x +=1
+        print('Enter numbers to submit seperated by commas, no spaces. For ex: 3,4,5,6')
+        selections = input('>> ')
+        selections = selections.split(',')
+        for num in selections:
+                url_split = paste_menu_selections[num].split('/')
+                url_num = url_split.pop()
+                print(url_num,paste)
+                requests.get('http://hotrack.pythonanywhere.com/submit/pastebin/{}/{}'.format(url_num,paste))
 
 pastes = retrieve_pastes()
+
 open_pastes(pastes)
+
+submit_pastes(pastes)
